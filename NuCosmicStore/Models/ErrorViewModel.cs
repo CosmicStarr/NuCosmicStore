ccb6b7f656628e3877c71355c434271418994d42
CosmicStarr <Normanj85@yahoo.com>
2024-07-20 13:37:40 -0500

    Add project files.

---
+++ NuCosmicStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NuCosmicStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
